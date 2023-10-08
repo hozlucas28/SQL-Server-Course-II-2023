e42c99587caa25ab41f13a4b9d61f9843150a078
hozlucas28 <hozlucas28@gmail.com>
2023-10-08 16:25:27 -0300

    Update init.sql

--- practical-work/part-01/init.sql
+++ practical-work/part-01/init.sql
@@ -1,11 +1,13 @@
-go
+GO
+USE [master];
+
+GO
 IF EXISTS (SELECT name FROM sys.databases WHERE name = 'cure_sa')
 BEGIN
-    USE [master];
     DROP DATABASE [cure_sa];
 END;
 
 CREATE DATABASE [cure_sa];
 
-go
+GO
 USE [cure_sa];
