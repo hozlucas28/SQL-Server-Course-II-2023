f446a67bf42a4e1b64bf69a53263b67ed83c063d
hozlucas28 <hozlucas28@gmail.com>
2023-10-10 18:30:36 -0300

    Add new schema

--- practical-work/part-01/schemas.sql
+++ practical-work/part-01/schemas.sql
@@ -2,6 +2,15 @@ GO
 USE [cure_sa];
 
 GO
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'archivos')
+BEGIN
+    DROP SCHEMA [archivos];
+END;
+
+GO
+CREATE SCHEMA [archivos];
+
+GO
 IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'datos')
 BEGIN
     DROP SCHEMA [datos];
