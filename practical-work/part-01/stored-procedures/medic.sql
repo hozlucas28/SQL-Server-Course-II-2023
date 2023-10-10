af7a883bd43251968ea7a4b80a05f2ead5a230c8
hozlucas28 <hozlucas28@gmail.com>
2023-10-10 17:42:58 -0300

    Create medic.sql

---
+++ practical-work/part-01/stored-procedures/medic.sql
@@ -0,0 +1,28 @@
+GO
+USE [cure_sa];
+
+-- Insertar médico
+GO
+CREATE OR ALTER PROCEDURE [datos].[insertarMedico]
+    @nombre VARCHAR(50) NOT NULL,
+    @apellido VARCHAR(50) NOT NULL,
+    @especialidad VARCHAR(50) NOT NULL,
+    @matricula INT NOT NULL
+AS
+BEGIN
+    DECLARE @idEspecialidad INT;
+
+    SET @idEspecialidad = [datos].[obtenerIdEspecialidad](@especialidad)
+
+    INSERT INTO [datos].[medicos]
+        (nombre, apellido, nro_matricula, id_especialidad)
+    VALUES
+        (@nombre, @apellido, @matricula, @idEspecialidad)
+END;
+
+-- Eliminar médico
+GO
+CREATE OR ALTER PROCEDURE [datos].[eliminarMedico]
+    @id INT
+AS
+    UPDATE [datos].[medicos] SET valido = 0 WHERE id_medico = @id
