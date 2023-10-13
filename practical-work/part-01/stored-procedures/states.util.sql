162e55a078c2d3d34013f0231c1301feec5e8a44
Alexis Castillo <alexis.fis02@gmail.com>
2023-10-13 20:38:28 -0300

    update states.utils and table [nombres].[provincias]

--- practical-work/part-01/stored-procedures/states.util.sql
+++ practical-work/part-01/stored-procedures/states.util.sql
@@ -1,7 +1,7 @@
 GO
 USE [cure_sa];
 
--- Obtener el ID de una provincia
+-- Obtener el ID de una provincia -- TODO: se usa?
 GO
 CREATE OR ALTER FUNCTION [referencias].[obtenerIdProvincia]
     (
@@ -27,11 +27,9 @@ BEGIN
     IF NULLIF(@provincia, '') IS NULL
         RETURN
 
-    IF NOT EXISTS (SELECT 1 FROM [referencias].[nombres_provincias] WHERE nombre = @provincia COLLATE Latin1_General_CS_AS) 
-        INSERT INTO [referencias].[nombres_provincias] (nombre) VALUES (@provincia)
-    ELSE
-        UPDATE [referencias].[nombres_provincias] SET nombre = @provincia WHERE nombre = @provincia COLLATE Latin1_General_CS_AS
+    IF NOT EXISTS (SELECT 1 FROM [referencias].[nombres_provincias] WHERE nombre = UPPER(@provincia) COLLATE Latin1_General_CS_AS) 
+        INSERT INTO [referencias].[nombres_provincias] (nombre) VALUES (UPPER(@provincia))
 
-    SELECT @outIdPais = id_pais, @outIdProvincia = id_provincia, @outNombre = nombre FROM [referencias].[nombres_provincias] WHERE nombre = @provincia COLLATE Latin1_General_CS_AS
+    SELECT @outIdPais = id_pais, @outIdProvincia = id_provincia, @outNombre = nombre FROM [referencias].[nombres_provincias] WHERE nombre = UPPER(@provincia) COLLATE Latin1_General_CS_AS
     RETURN
 END;
