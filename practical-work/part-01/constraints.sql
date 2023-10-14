ef605e5680a4891d40e9d8d8bbbd96d445f21dc6
Alexis Castillo <alexis.fis02@gmail.com>
2023-10-14 19:42:29 -0300

    update constraints

--- practical-work/part-01/constraints.sql
+++ practical-work/part-01/constraints.sql
@@ -8,14 +8,10 @@ ALTER TABLE [referencias].[generos] ADD CONSTRAINT pk_id_genero PRIMARY KEY (id_
 -- Paises
 ALTER TABLE [referencias].[paises] ADD CONSTRAINT pk_id_pais PRIMARY KEY (id_pais);
 
--- Nombres de localidades
-ALTER TABLE [referencias].[nombres_localidades] ADD CONSTRAINT pk_id_localidad PRIMARY KEY (id_localidad);
+ALTER TABLE [referencias].[nacionalidades] ADD CONSTRAINT pk_id_nacionalidad PRIMARY KEY (id_nacionalidad);
 
 -- Tipos de documentos
 ALTER TABLE [referencias].[tipos_documentos] ADD CONSTRAINT pk_id_tipo_documento PRIMARY KEY (id_tipo_documento);
-
--- Coverturas
-ALTER TABLE [datos].[coberturas] ADD CONSTRAINT pk_id_cobertura PRIMARY KEY (id_cobertura);
 
 -- Especialidad
 ALTER TABLE [datos].[especialidad] ADD CONSTRAINT pk_id_especialidad PRIMARY KEY (id_especialidad);
@@ -24,11 +20,23 @@ ALTER TABLE [datos].[especialidad] ADD CONSTRAINT pk_id_especialidad PRIMARY KEY
 ALTER TABLE [referencias].[nombres_provincias] ADD CONSTRAINT pk_id_provincia PRIMARY KEY (id_provincia),
     CONSTRAINT fk_id_pais_nombres_provincias FOREIGN KEY (id_pais) REFERENCES [referencias].[paises] (id_pais);
 
+-- Nombres de localidades
+ALTER TABLE [referencias].[nombres_localidades] ADD CONSTRAINT pk_id_localidad PRIMARY KEY (id_localidad),
+    CONSTRAINT fk_id_provincia_localidades FOREIGN KEY (id_provincia) REFERENCES [referencias].[nombres_provincias] (id_provincia) ;
+
+
 -- Direcciones
 ALTER TABLE [referencias].[direcciones] ADD CONSTRAINT pk_id_direccion PRIMARY KEY (id_direccion),
     CONSTRAINT fk_id_pais_direcciones FOREIGN KEY (id_pais) REFERENCES [referencias].[paises] (id_pais),
     CONSTRAINT fk_id_provincia FOREIGN KEY (id_provincia) REFERENCES [referencias].[nombres_provincias] (id_provincia),
     CONSTRAINT fk_id_localidad FOREIGN KEY (id_localidad) REFERENCES [referencias].[nombres_localidades] (id_localidad);
+
+-- Prestadores
+ALTER TABLE [datos].[prestadores] ADD CONSTRAINT pk_id_prestador PRIMARY KEY (id_prestador);
+
+-- Coverturas
+ALTER TABLE [datos].[coberturas] ADD CONSTRAINT pk_id_cobertura PRIMARY KEY (id_cobertura),
+    CONSTRAINT fk_id_prestador_coberturas FOREIGN KEY (id_prestador) REFERENCES [datos].[prestadores] (id_prestador);
 
 -- Pacientes
 ALTER TABLE [datos].[pacientes] ADD CONSTRAINT pk_id_paciente PRIMARY KEY (id_paciente),
@@ -36,7 +44,7 @@ ALTER TABLE [datos].[pacientes] ADD CONSTRAINT pk_id_paciente PRIMARY KEY (id_pa
     CONSTRAINT fk_id_direccion_pacientes FOREIGN KEY (id_direccion) REFERENCES [referencias].[direcciones] (id_direccion),
     CONSTRAINT fk_id_tipo_documento FOREIGN KEY (id_tipo_documento) REFERENCES [referencias].[tipos_documentos] (id_tipo_documento),
     CONSTRAINT fk_id_genero FOREIGN KEY (id_genero) REFERENCES [referencias].[generos] (id_genero),
-    CONSTRAINT fk_id_nacionalidad FOREIGN KEY (nacionalidad) REFERENCES [referencias].[paises] (id_pais);
+    CONSTRAINT fk_id_nacionalidad FOREIGN KEY (nacionalidad) REFERENCES [referencias].[nacionalidades] (id_nacionalidad);
 
 -- Estudios
 ALTER TABLE [datos].[estudios] ADD CONSTRAINT pk_id_estudio PRIMARY KEY (id_estudio),
@@ -45,10 +53,6 @@ ALTER TABLE [datos].[estudios] ADD CONSTRAINT pk_id_estudio PRIMARY KEY (id_estu
 -- Usuarios
 ALTER TABLE [datos].[usuarios] ADD CONSTRAINT pk_id_usuario PRIMARY KEY (id_usuario),
     CONSTRAINT fk_id_paciente_usuario FOREIGN KEY (id_paciente) REFERENCES [datos].[pacientes] (id_paciente);
-
--- Prestadores
-ALTER TABLE [datos].[prestadores] ADD CONSTRAINT pk_id_prestador PRIMARY KEY (id_prestador),
-    CONSTRAINT fk_id_cobertura FOREIGN KEY (id_cobertura) REFERENCES [datos].[coberturas] (id_cobertura);
 
 -- Sedes de atención
 ALTER TABLE [datos].[sede_de_atencion] ADD CONSTRAINT pk_id_medico_sede_de_atención PRIMARY KEY (id_sede),
@@ -77,5 +81,7 @@ ALTER TABLE [datos].[reservas_turnos_medicos] ADD CONSTRAINT pk_id_turno PRIMARY
     CONSTRAINT fk_id_estado FOREIGN KEY (id_tipo_turno) REFERENCES [datos].[estados_turnos] (id_estado),
     CONSTRAINT fk_id_paciente_turno FOREIGN KEY (id_paciente) REFERENCES [datos].[pacientes] (id_paciente),
     CONSTRAINT fk_id_dias_x_sede FOREIGN KEY (id_dias_x_sede) REFERENCES [datos].[dias_x_sede] (id_dias_x_sede);
+
+go
 
 use master;
