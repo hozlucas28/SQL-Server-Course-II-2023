70d130696680e35a82608c166ac5a0120907c635
hozlucas28 <hozlucas28@gmail.com>
2023-11-13 23:52:24 -0300

    Rename and Update schemas.sql

---
+++ practical-work/src/02-schemas.sql
@@ -0,0 +1,31 @@
+USE [CURESA];
+GO
+
+/* ---------------------------- Eliminar Esquemas --------------------------- */
+
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'archivos')
+    DROP SCHEMA [archivos];
+
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'datos')
+    DROP SCHEMA [datos];
+
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'referencias')
+    DROP SCHEMA [referencias];
+
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'utils')
+    DROP SCHEMA [utils]
+GO
+
+
+/* ----------------------------- Crear Esquemas ----------------------------- */
+
+CREATE SCHEMA [archivos];
+GO
+
+CREATE SCHEMA [datos];
+GO
+
+CREATE SCHEMA [referencias];
+GO
+
+CREATE SCHEMA [utils];
