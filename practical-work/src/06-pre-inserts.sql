891c95719727d0da87e3b767f1aa075c0a9cbe0d
Gonza <gonzalososa342@gmail.com>
2023-11-14 12:19:23 -0300

    Minimal changes, drop constraints

--- practical-work/src/06-pre-inserts.sql
+++ practical-work/src/06-pre-inserts.sql
@@ -12,4 +12,5 @@ GO
 /* ---------------------------- Crear Inserciones --------------------------- */
 
 INSERT INTO [datos].[tipos_turnos] ([nombre_tipo]) VALUES ('PRESENCIAL'), ('VIRTUAL');
-INSERT INTO [datos].[estados_turnos] ([nombre]) VALUES ('PENDIENTE'), ('ATENDIDO'), ('AUSENTE'), ('CANCELADO');
+
+INSERT INTO [datos].[estados_turnos] ([nombre]) VALUES ('ATENDIDO'), ('AUSENTE'), ('CANCELADO'),('PENDIENTE');
