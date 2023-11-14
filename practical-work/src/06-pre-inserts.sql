617cabe288d23ddd481edb977b746b293599b498
hozlucas28 <hozlucas28@gmail.com>
2023-11-13 23:47:21 -0300

    Create 06-pre-inserts.sql

---
+++ practical-work/src/06-pre-inserts.sql
@@ -0,0 +1,14 @@
+USE [CURESA];
+GO
+
+/* -------------------------- Eliminar Inserciones -------------------------- */
+
+DELETE FROM [datos].[tipos_turnos];
+DELETE FROM [datos].[estados_turnos];
+GO
+
+
+/* ---------------------------- Crear Inserciones --------------------------- */
+
+INSERT INTO [datos].[tipos_turnos] ([nombre_tipo]) VALUES ('PRESENCIAL'), ('VIRTUAL');
+INSERT INTO [datos].[estados_turnos] ([nombre]) VALUES ('PENDIENTE'), ('ATENDIDO'), ('AUSENTE'), ('CANCELADO');
