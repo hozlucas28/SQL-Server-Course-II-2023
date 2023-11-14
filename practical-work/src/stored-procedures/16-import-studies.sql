ec708ec369920af3b39f43d7788775a4877f903d
hozlucas28 <hozlucas28@gmail.com>
2023-11-14 00:41:14 -0300

    Rename and Update import-studies.sql

--- practical-work/src/stored-procedures/16-import-studies.sql
+++ practical-work/src/stored-procedures/16-import-studies.sql
@@ -1,33 +1,36 @@
-USE [CURESA]
+USE [CURESA];
 GO
 
--- TODO: buscar la forma de que [estudio] guarde los registros con acentos
 
+/* ------------- Procedimientos Almacenados - Importar Estudios ------------- */
+
+-- TODO: Buscar la forma de que [estudio] guarde los registros con acentos.
+-- Importar estudios desde un archivo JSON
 CREATE OR ALTER PROCEDURE [archivos].[importarEstudiosJSON]
     @rutaArchivo VARCHAR(255)
 AS
 BEGIN
-    DECLARE @error NVARCHAR(255);
-    DECLARE @sql NVARCHAR(MAX);
-    DECLARE @json NVARCHAR(MAX);
-    DECLARE @paramDef NVARCHAR(100);
+    DECLARE @error NVARCHAR(255)
+    DECLARE @sql NVARCHAR(MAX)
+    DECLARE @json NVARCHAR(MAX)
+    DECLARE @paramDef NVARCHAR(100)
     
     IF LEN(@rutaArchivo) = 0
     BEGIN
         SET @error = 'La ruta del archivo JSON no puede estar vacía.';
-        THROW 51001, @error, 1;
-        RETURN;
+        THROW 51001, @error, 1
+        RETURN
     END
 
     SET @sql = N'
         SET @json_string = (SELECT * FROM OPENROWSET (BULK ''' + @rutaArchivo + ''', SINGLE_CLOB) as JsonFile)
     '
     SET @paramDef = N'
-        @json_string NVARCHAR(MAX) OUTPUT';  
+        @json_string NVARCHAR(MAX) OUTPUT'
     
-    EXEC sp_executesql @sql, @paramDef, @json_string = @json OUTPUT;
+    EXEC sp_executesql @sql, @paramDef, @json_string = @json OUTPUT
 
-    IF OBJECT_ID('tempdb..#EstudiosMedicos') IS NOT NULL 
+    IF OBJECT_ID('tempdb..[#EstudiosMedicos]') IS NOT NULL 
         DROP TABLE [#EstudiosMedicos]
 
     CREATE TABLE [#EstudiosMedicos]
@@ -89,13 +92,12 @@ BEGIN
         INNER JOIN [datos].[prestadores] p ON [em].[prestador] = [p].[nombre]
         WHERE [p].[plan_prestador] = [em].[plan]
 
-        DROP TABLE [#EstudiosMedicos];
+        DROP TABLE [#EstudiosMedicos]
     END TRY
     BEGIN CATCH
         DECLARE @errorMessage NVARCHAR(1000)
 		SET @errorMessage = ERROR_MESSAGE()
 		PRINT 'Error durante la inserción: ' + @errorMessage;
-        THROW;
+        THROW
     END CATCH
 END;
-
