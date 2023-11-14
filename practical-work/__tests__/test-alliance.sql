4bd455364842dd2c800518778a55546d0956dc2b
hozlucas28 <hozlucas28@gmail.com>
2023-11-14 16:10:12 -0300

    Update alliance.sql

--- practical-work/__tests__/test-alliance.sql
+++ practical-work/__tests__/test-alliance.sql
@@ -1,38 +1,22 @@
-USE CURESA;
+USE [CURESA];
 GO
 
-select * from datos.reservas_turnos_medicos
 
-DECLARE
-@idPaciente INT,
-@idPrestador INT,
-@idCobertura INT;
+/* ---------------------------- Verificar Alianza --------------------------- */
 
-SET @idPaciente = 137;
-SET @idPrestador = 1;
+SELECT * FROM [datos].[reservas_turnos_medicos];
 
-INSERT INTO datos.coberturas
-(id_prestador, imagen_credencial, nro_socio)
-VALUES(@idPrestador, null, @idPaciente)
+DECLARE @idPaciente INT = 137;
+DECLARE @idPrestador INT = 1;
+DECLARE @idCobertura INT;
 
-SELECT @idCobertura = id_cobertura
-FROM datos.coberturas
-WHERE id_prestador = @idPrestador
+INSERT INTO [datos].[coberturas] ([id_prestador], [imagen_credencial], [nro_socio]) VALUES (@idPrestador, null, @idPaciente);
 
-SELECT * FROM 
-datos.pacientes
-WHERE id_paciente = @idPaciente
+SELECT @idCobertura = [id_cobertura] FROM [datos].[coberturas] WHERE [id_prestador] = @idPrestador;
+SELECT * FROM [datos].[pacientes] WHERE [id_paciente] = @idPaciente;
 
-UPDATE datos.pacientes 
-SET id_cobertura = @idCobertura
-WHERE id_paciente = @idPaciente
+UPDATE [datos].[pacientes] SET [id_cobertura] = @idCobertura WHERE [id_paciente] = @idPaciente;
+SELECT * FROM [datos].[reservas_turnos_medicos] WHERE [id_paciente] = @idPaciente;
 
-SELECT * FROM datos.reservas_turnos_medicos
-WHERE id_paciente = @idPaciente;
-
-DELETE FROM datos.prestadores
-WHERE id_prestador = @idPrestador
-
-SELECT * FROM datos.reservas_turnos_medicos
-WHERE id_paciente = @idPaciente;
-
+DELETE FROM [datos].[prestadores] WHERE [id_prestador] = @idPrestador;
+SELECT * FROM [datos].[reservas_turnos_medicos] WHERE [id_paciente] = @idPaciente;
