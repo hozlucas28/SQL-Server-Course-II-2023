a12888199c7ddba25545f709d365a6ad3bb7ed95
hozlucas28 <hozlucas28@gmail.com>
2023-11-14 00:35:27 -0300

    Rename and Update districts.util.sql

--- practical-work/src/stored-procedures/02-districts.util.sql
+++ practical-work/src/stored-procedures/02-districts.util.sql
@@ -1,23 +1,25 @@
-GO
 USE [CURESA];
+GO
+
+
+/* ---------------- Procedimientos Almacenados - Localidades ---------------- */
 
 -- Obtener el ID de una localidad
-GO
 CREATE OR ALTER PROCEDURE [referencias].[obtenerOInsertarIdLocalidad]
     @Localidad VARCHAR(255),
 	@IdProvincia INT,
 	@IdLocalididad INT OUTPUT
 AS
 BEGIN
-    SET @Localidad = UPPER(@Localidad);
+    SET @Localidad = UPPER(@Localidad)
     IF NOT EXISTS (SELECT 1 FROM [referencias].[nombres_localidades] WHERE [id_provincia] = @IdProvincia AND [nombre] = @Localidad)
-        INSERT INTO [referencias].[nombres_localidades] ([nombre],[id_provincia]) VALUES (@Localidad,@IdProvincia);
+        INSERT INTO [referencias].[nombres_localidades] ([nombre],[id_provincia]) VALUES (@Localidad,@IdProvincia)
 
-	SELECT @IdLocalididad = [id_localidad] FROM [referencias].[nombres_localidades] WHERE [id_provincia] = @IdProvincia AND [nombre] = @Localidad;
+	SELECT @IdLocalididad = [id_localidad] FROM [referencias].[nombres_localidades] WHERE [id_provincia] = @IdProvincia AND [nombre] = @Localidad
 END;
+GO
 
 -- Actualizar/Insertar una localidad
-GO
 CREATE OR ALTER PROCEDURE [referencias].[insertarLocalidad]
     @localidad VARCHAR(50) = 'null',
     @outIdLocalidad INT OUTPUT,
@@ -33,5 +35,4 @@ BEGIN
         UPDATE [referencias].[nombres_localidades] SET [nombre] = @localidad WHERE [nombre] = @localidad
 
     SELECT @outIdLocalidad = [id_localidad], @outNombre = [nombre] FROM [referencias].[nombres_localidades] WHERE [nombre] = @localidad
-    RETURN
 END;
