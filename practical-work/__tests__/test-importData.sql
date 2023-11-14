4c23b6b0cf436481b99295f3611845f96706bb98
hozlucas28 <hozlucas28@gmail.com>
2023-11-14 16:03:48 -0300

    Update importData.sql

--- practical-work/__tests__/test-importData.sql
+++ practical-work/__tests__/test-importData.sql
@@ -1,49 +1,28 @@
-USE [CURESA]
+USE [CURESA];
 GO
 
+/* ----------------------------- Importar Datos ----------------------------- */
+
 -- Pacientes
-
-EXEC [archivos].importarPacientesCSV 
-    @rutaArchivo = "C:\importar\Pacientes.csv";
-
-select top 10 
+EXEC [archivos].[importarPacientesCSV] @rutaArchivo = "C:\importar\Pacientes.csv";
+SELECT TOP 10 * FROM [datos].[pacientes];
+GO
 
 -- Prestadores
+EXEC [archivos].[importarPrestadoresCSV] @rutaArchivo = "C:\importar\Prestador.csv";
+SELECT TOP 10 * FROM [datos].[prestadores];
+GO
 
-EXEC [archivos].importarPrestadoresCSV 
-    @rutaArchivo = "C:\importar\Prestador.csv"
-
-select top 10 from datos.prestadores
-
--- Medicos
-
-EXEC [archivos].[importarMedicosCSV]
-    @rutaArchivo = "C:\importar\Medicos.csv";
-
-select top 10 from datos.medicos
+-- Médicos
+EXEC [archivos].[importarMedicosCSV] @rutaArchivo = "C:\importar\Medicos.csv";
+SELECT TOP 10 * FROM [datos].[medicos];
+GO
 
 -- Sedes
+EXEC [archivos].[importarSedesCSV] @rutaArchivo = "C:\importar\Sedes.csv";
+SELECT TOP 10 * FROM [datos].[sede_de_atencion];
 
-EXEC [archivos].importarSedesCSV
-    @rutaArchivo = "C:\importar\Sedes.csv";
-
-select top 10 from datos.sede_de_atencion
-
--- Estudios Validos [Personal Técnico Clínico]
-
-EXEC [archivos].[importarEstudiosJSON]
-    @rutaArchivo = "C:\importar\Centro_Autorizaciones.Estudios clinicos.json";
-
-select top 10 from datos.estudiosValidos
-
--- Borrar
-
--- delete from datos.[pacientes];
-
--- delete from datos.[prestadores];
-
--- delete from datos.[medicos];
-
--- delete from datos.[sede_de_atencion];
-
--- delete from datos.estudiosValidos
+-- Estudios válidos
+EXEC [archivos].[importarEstudiosJSON] @rutaArchivo = "C:\importar\Centro_Autorizaciones.Estudios clinicos.json";
+SELECT TOP 10 * FROM [datos].[estudiosValidos]
+GO
