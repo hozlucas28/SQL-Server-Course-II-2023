6937de59fe9fdd7c52fd41d5c213bf182428d3b0
hozlucas28 <hozlucas28@gmail.com>
2023-11-14 00:36:37 -0300

    Rename and Update genders.util.sql

--- practical-work/src/stored-procedures/06-genders.util.sql
+++ practical-work/src/stored-procedures/06-genders.util.sql
@@ -1,29 +1,30 @@
 USE [CURESA];
 GO
 
--- Obtener el ID de un género
 
+/* ------------------ Procedimientos Almacenados - Géneros ------------------ */
+
+-- Obtener el ID de un género
 CREATE OR ALTER PROCEDURE [referencias].[obtenerOInsertarIdGenero]
     @nombre VARCHAR(50) = 'null',
     @id INT OUTPUT 
 AS
 BEGIN
-    SET @nombre = UPPER(TRIM(@nombre));
+    SET @nombre = UPPER(TRIM(@nombre))
 
     IF NULLIF(@nombre, '') IS NULL
-        SET @id = -1;
+        SET @id = -1
     ELSE
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM [referencias].[generos] WHERE UPPER(TRIM([nombre])) = @nombre) 
-            INSERT INTO [referencias].[generos] ([nombre]) VALUES (@nombre);
+            INSERT INTO [referencias].[generos] ([nombre]) VALUES (@nombre)
 
-        SELECT @id = [id_genero] FROM [referencias].[generos] WHERE UPPER(TRIM([nombre])) = @nombre;
+        SELECT @id = [id_genero] FROM [referencias].[generos] WHERE UPPER(TRIM([nombre])) = @nombre
     END
 END;
 GO
 
 -- Actualizar/Insertar un género
-
 CREATE OR ALTER PROCEDURE [referencias].[actualizarGenero]
     @nombre VARCHAR(50) = 'null',
     @outIdGenero INT OUTPUT,
@@ -38,5 +39,5 @@ BEGIN
     ELSE
         UPDATE [referencias].[generos] SET [nombre] = @nombre WHERE [nombre] = @nombre
 
-    SELECT @outIdGenero = [id_genero], @outNombre = [nombre] FROM [referencias].[generos] WHERE [nombre] = @nombre;
+    SELECT @outIdGenero = [id_genero], @outNombre = [nombre] FROM [referencias].[generos] WHERE [nombre] = @nombre
 END;
