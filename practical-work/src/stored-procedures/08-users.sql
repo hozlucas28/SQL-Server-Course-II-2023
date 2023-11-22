48efe0d706c137460079f0e9e1706ed4bc2fdd36
hozlucas28 <hozlucas28@gmail.com>
2023-11-22 02:37:42 -0300

    Add new procedures

--- practical-work/src/stored-procedures/08-users.sql
+++ practical-work/src/stored-procedures/08-users.sql
@@ -4,11 +4,33 @@ GO
 
 /* ------------------ Procedimientos Almacenados - Usuarios ----------------- */
 
--- Actualizar contraseña
-CREATE OR ALTER PROCEDURE [datos].[actualizarContraseña]
-    @idUsuario INT,
-    @contraseña VARCHAR(256)
+-- Insertar usuario
+CREATE OR ALTER PROCEDURE [datos].[insertarUsuario]
+	@contraseña VARCHAR(256),
+    @idPaciente INT = NULL
 AS
 BEGIN
-    UPDATE [datos].[usuarios] SET [contraseña] = @contraseña WHERE [id_usuario] = @idUsuario
+    INSERT INTO [datos].[usuarios] ([contraseña], [id_paciente]) VALUES (@contraseña, @idPaciente)
 END;
+GO
+
+-- Actualizar usuario
+CREATE OR ALTER PROCEDURE [datos].[actualizarUsuario]
+    @contraseña VARCHAR(256) = NULL,
+    @idPaciente INT = NULL,
+    @idUsuario INT
+AS
+BEGIN
+    UPDATE [datos].[usuarios] SET
+        [contraseña] = ISNULL(@contraseña, [contraseña]),
+        [id_paciente] = ISNULL(@idPaciente, [id_paciente])
+    WHERE
+        [id_usuario] = @idUsuario
+END;
+GO
+
+-- Borrar usuario
+CREATE OR ALTER PROCEDURE [datos].[borrarUsuario]
+    @id INT
+AS
+    DELETE FROM [datos].[usuarios] WHERE [id_usuario] = @id
