00dfc6561109ec9a8170b40b3d46431f76f94741
hozlucas28 <hozlucas28@gmail.com>
2023-11-22 02:32:43 -0300

    Update updatePassword.sql test

--- practical-work/__tests__/test-updatePassword.sql
+++ practical-work/__tests__/test-updatePassword.sql
@@ -1,14 +1,23 @@
 USE [CURESA];
 GO
 
+
+/* -------------------------- Actualizar Contraseña ------------------------- */
+
+DECLARE @nuevaContra VARCHAR(255) = 'LaYutaExistePorUstedes62-23';
+DECLARE @viejaContra VARCHAR(255) = '12345';
+DECLARE @id_usuario INT = NULL;
+
+-- Eliminar usuario
+SELECT @id_usuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @nuevaContra;
+IF @id_usuario IS NOT NULL
+    EXEC [datos].[borrarUsuario] @id = @id_usuario;
+
+-- Crear usuario
+EXEC [datos].[insertarUsuario] @contraseña = @viejaContra;
+SELECT * FROM [datos].[usuarios] WHERE [contraseña] = @viejaContra;
+
 -- Actualizar contraseña del usuario
-DECLARE @contraseñaOriginal VARCHAR(255) = '12345';
-DECLARE @contraseñaCambiada VARCHAR(255) = 'LaYutaExistePorUstedes62-23';
-DECLARE @idUsuario INT;
-
-INSERT INTO [datos].[usuarios] ([contraseña]) values (@contraseñaOriginal);
-SELECT @idUsuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @contraseñaOriginal;
-EXEC [datos].[actualizarContraseña] @idUsuario, @contraseñaCambiada;
-
--- Mostrar usuario actualizado
-SELECT * FROM [datos].[usuarios] WHERE [id_usuario] = @idUsuario;
+SELECT @id_usuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @viejaContra;
+EXEC [datos].[actualizarUsuario] @idUsuario = @id_usuario, @contraseña = @nuevaContra;
+SELECT * FROM [datos].[usuarios] WHERE [id_usuario] = @id_usuario;
