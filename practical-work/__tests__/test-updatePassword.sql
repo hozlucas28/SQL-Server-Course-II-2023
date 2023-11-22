39488c16cd361e91e3cd3e850b3a6cb4fd7b05ac
hozlucas28 <hozlucas28@gmail.com>
2023-11-22 03:49:13 -0300

    Update updatePassword test

--- practical-work/__tests__/test-updatePassword.sql
+++ practical-work/__tests__/test-updatePassword.sql
@@ -1,23 +1,27 @@
 USE [CURESA];
 GO
 
+/*
+    Se requiere la ejecución previa de la semilla: < seed.sql >
+*/
+
 
 /* -------------------------- Actualizar Contraseña ------------------------- */
 
 DECLARE @nuevaContra VARCHAR(255) = 'LaYutaExistePorUstedes62-23';
 DECLARE @viejaContra VARCHAR(255) = '12345';
-DECLARE @id_usuario INT = NULL;
+DECLARE @idUsuario INT = NULL;
 
 -- Limpiar registros del test
-SELECT @id_usuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @nuevaContra;
-IF @id_usuario IS NOT NULL
-    EXEC [datos].[borrarUsuario] @id = @id_usuario;
+SELECT @idUsuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @nuevaContra;
+IF @idUsuario IS NOT NULL
+    EXEC [datos].[borrarUsuario] @id = @idUsuario;
 
 -- Crear usuario
 EXEC [datos].[insertarUsuario] @contraseña = @viejaContra;
 SELECT * FROM [datos].[usuarios] WHERE [contraseña] = @viejaContra;
 
 -- Actualizar contraseña del usuario
-SELECT @id_usuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @viejaContra;
-EXEC [datos].[actualizarUsuario] @idUsuario = @id_usuario, @contraseña = @nuevaContra;
-SELECT * FROM [datos].[usuarios] WHERE [id_usuario] = @id_usuario;
+SELECT @idUsuario = [id_usuario] FROM [datos].[usuarios] WHERE [contraseña] = @viejaContra;
+EXEC [datos].[actualizarUsuario] @idUsuario = @idUsuario, @contraseña = @nuevaContra;
+SELECT * FROM [datos].[usuarios] WHERE [id_usuario] = @idUsuario;
